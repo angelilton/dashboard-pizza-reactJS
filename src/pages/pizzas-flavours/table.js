536c5da250d2c63afee2d8cf993ffef84285dc83
angelilton <epifanio.rds@gmail.com>
2021-02-15 22:46:03 +0000

    add table body

--- src/pages/pizzas-flavours/table.js
+++ src/pages/pizzas-flavours/table.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link, useRouteMatch } from 'react-router-dom'
 import {
   Button,
   Grid,
+  List,
+  ListItem as MaterialListItem,
+  ListItemText,
   Table,
   TableBody,
   TableCell,
@@ -41,8 +45,77 @@ const TablePizzaFlavours = () => {
           </Button>
         </Grid>
       </TableTitleContainer>
+
+      <Table>
+        <THead>
+          <TableRow>
+            <Th>Foto</Th>
+            <Th>Nome</Th>
+            <Th>Valores</Th>
+            <Th />
+          </TableRow>
+        </THead>
+
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <img
+                src=""
+                alt=""
+                style={{
+                  background: '#fc0',
+                  display: 'block',
+                  height: '70px',
+                  width: '70px'
+                }}
+              />
+            </TableCell>
+
+            <TableCell>Sabor da pizza</TableCell>
+
+            <TableCell>
+              <List>
+                <ListItem name={'Média'} value={30} />
+                <ListItem name={'pequena'} value={20} />
+                <ListItem name={'Broto'} value={10} />
+              </List>
+            </TableCell>
+
+            <TableCell>
+              <TableButton
+                startIcon={<Edit />}
+                component={Link}
+                to={`${PIZZAS_FLAVOURS}${EDIT(1)}`}
+              >
+                Editar
+              </TableButton>
+
+              <TableButton
+                color="secondary"
+                startIcon={<Delete />}
+                onClick={() => {}}
+              >
+                Remover
+              </TableButton>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </TableContainer>
   )
+}
+
+const ListItem = ({ name, value }) => (
+  <MaterialListItem>
+    <ListItemText>
+      <strong>{name}</strong>: R$ {value}
+    </ListItemText>
+  </MaterialListItem>
+)
+
+ListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.isRequired
 }
 
 export default TablePizzaFlavours
