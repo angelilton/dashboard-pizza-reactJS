f12777674da5b4ae2adb11b0919b43a6b177e655
angelilton <epifanio.rds@gmail.com>
2021-02-16 17:05:42 +0000

    display pizza names & images

--- src/pages/pizzas-flavours/table.js
+++ src/pages/pizzas-flavours/table.js
@@ -22,9 +22,13 @@ import {
   Th
 } from 'ui'
 import { PIZZAS_FLAVOURS, NEW, EDIT } from 'routes'
+import { useCollection } from 'hooks'
 
 const TablePizzaFlavours = () => {
   const newFlavourPath = useRouteMatch(`${PIZZAS_FLAVOURS}${NEW}`)
+  const { data: pizzasFlavours, remove } = useCollection('pizzasFlavours')
+
+  console.log('flavours:', pizzasFlavours)
 
   return (
     <TableContainer>
@@ -57,55 +61,48 @@ const TablePizzaFlavours = () => {
         </THead>
 
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <img
-                src=""
-                alt=""
-                style={{
-                  background: '#fc0',
-                  display: 'block',
-                  height: '70px',
-                  width: '70px'
-                }}
-              />
-            </TableCell>
+          {pizzasFlavours?.map((pizza) => (
+            <TableRow key={pizza.id}>
+              <TableCell>
+                <img src={pizza.image} alt={pizza.name} width="100" />
+              </TableCell>
 
-            <TableCell>Sabor da pizza</TableCell>
+              <TableCell>{pizza.name}</TableCell>
 
-            <TableCell>
-              <List>
-                <ListItem name={'Média'} value={30} />
-                <ListItem name={'pequena'} value={20} />
-                <ListItem name={'Broto'} value={10} />
-              </List>
-            </TableCell>
+              <TableCell>
+                <List>
+                  <ListItem name={'Média'} value={30} />
+                  <ListItem name={'pequena'} value={20} />
+                  <ListItem name={'Broto'} value={10} />
+                </List>
+              </TableCell>
 
-            <TableCell>
-              <TableButton
-                startIcon={<Edit />}
-                component={Link}
-                to={`${PIZZAS_FLAVOURS}${EDIT(1)}`}
-              >
-                Editar
-              </TableButton>
+              <TableCell>
+                <TableButton
+                  startIcon={<Edit />}
+                  component={Link}
+                  to={`${PIZZAS_FLAVOURS}${EDIT(pizza.id)}`}
+                >
+                  Editar
+                </TableButton>
 
-              <TableButton
-                color="secondary"
-                startIcon={<Delete />}
-                onClick={() => {}}
-              >
-                Remover
-              </TableButton>
-            </TableCell>
-          </TableRow>
+                <TableButton
+                  color="secondary"
+                  startIcon={<Delete />}
+                  onClick={() => remove(pizza.id)}
+                >
+                  Remover
+                </TableButton>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
   )
 }
 
-const ListItem = ({ name, value }) => (
+const ListItem = ({ name = '', value }) => (
   <MaterialListItem>
     <ListItemText>
       <strong>{name}</strong>: R$ {value}
@@ -114,8 +111,8 @@ const ListItem = ({ name, value }) => (
 )
 
 ListItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  value: PropTypes.isRequired
+  name: PropTypes.string,
+  value: PropTypes.number.isRequired
 }
 
 export default TablePizzaFlavours
